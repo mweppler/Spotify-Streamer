df2927cf8733654f7393cf69cac904675e2f471a
Matt Weppler <mweppler@gmail.com>
2015-06-08 10:41:36 -0700

    feat: started building artist search activity
    Built out view components for the artist search activity:
    
    * search view
    * list view (artist search results - dummy data for now)
      * artist thumbnail (image view)
      * artist name (text view)
    
    Wired up the on item select on the list view. Currently just showing
    toast with the artist name.

--- app/src/main/java/me/weppler/android/spotifystreamer/ArtistSearchActivityFragment.java
+++ app/src/main/java/me/weppler/android/spotifystreamer/ArtistSearchActivityFragment.java
@@ -1,16 +1,27 @@
 package me.weppler.android.spotifystreamer;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ArtistSearchActivityFragment extends Fragment {
+    private final String TAG = ArtistSearchActivityFragment.class.getSimpleName();
+    private ArrayAdapter<String> mArtistAdapter;
 
     public ArtistSearchActivityFragment() {
     }
@@ -18,6 +29,31 @@ public class ArtistSearchActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_artist_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_artist_search, container, false);
+        String[] artistArray = {
+                "Coldplay",
+                "The Doors",
+                "In Truth",
+                "Geto Boys",
+                "504 Boyz",
+                "Porcelain Raft"
+        };
+        List<String> artistArrayList = new ArrayList<String>(Arrays.asList(artistArray));
+        mArtistAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.artist_list_item,
+                R.id.artist_name_text_view,
+                artistArrayList
+        );
+        final ListView listView = (ListView) rootView.findViewById(R.id.artist_list_view);
+        listView.setAdapter(mArtistAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String artist = mArtistAdapter.getItem(position);
+                Toast.makeText(getActivity(), artist, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
     }
 }
